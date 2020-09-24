8ff4f414d2078425ea1ebf78ded02750cf509eca
b00mman <vincent.jude.clement@gmail.com>
2020-09-24 11:17:23 -0700

    added day length variable
    switched the length of the day for a variable we can change later.

--- After Dawn/Assets/Scripts/DayNightCycle.cs
+++ After Dawn/Assets/Scripts/DayNightCycle.cs
@@ -7,10 +7,11 @@ public class DayNightCycle : MonoBehaviour
     public static float sunset;
     private bool night = false;
     private Vector3 rot;
+    private float dayLength = 300;
     // Start is called before the first frame update
     void Start()
     {
-        sunset = Time.time + 300;
+        sunset = Time.time + dayLength;
         rot = transform.rotation.eulerAngles;
     }
 
@@ -20,10 +21,10 @@ public class DayNightCycle : MonoBehaviour
         if (Time.time >= sunset)
         {
             night = !night;
-            sunset = Time.time + 300;
+            sunset = Time.time + dayLength;
         }
 
-        rot.z = ((sunset - Time.time) / 300) * 180;
+        rot.z = ((sunset - Time.time) / dayLength) * 180;
         if (!night){
             rot.z += 180;
         }
