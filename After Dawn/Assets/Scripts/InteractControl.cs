d16beb1b431dc9f8b573e449c9a5ce6f6f1fe8e3
John Jorgensen <john@jorgensenfamily.us>
2020-09-25 18:32:42 -0700

    Interactions
    Created a new script (attached to the main camera) that calls a function for interactions based on the name of the object clicked (only if the tag is "Interactables"

---
+++ After Dawn/Assets/Scripts/InteractControl.cs
@@ -0,0 +1,43 @@
+﻿using UnityEditor.UI;
+using UnityEngine;
+
+public class InteractControl : MonoBehaviour
+{
+    string InteractName;
+
+    Camera camera;
+    private void Start()
+    {
+        camera = GetComponent<Camera>();
+    }
+
+    void Update()
+    {
+        Interaction();
+    }
+
+    void Interaction() //Controls calling of interaction functions
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+			Ray interact = camera.ScreenPointToRay(Input.mousePosition);
+			RaycastHit interactInfo;
+
+			if (Physics.Raycast(interact, out interactInfo, 5))
+			{
+				if (interactInfo.collider.gameObject.tag == "Interactable")
+				{
+                    InteractName = interactInfo.collider.gameObject.name;
+                    Invoke(InteractName, 0);
+                }
+			}
+		}
+    }
+
+
+    //Place functions with the same name as the object you are interacting with here.
+    void Box()
+    {
+        print("You opened a box");
+    }
+}
