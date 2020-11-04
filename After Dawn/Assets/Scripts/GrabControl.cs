8e2b0193c8593ce1805873bd9e489a9f926fb00c
John Jorgensen <john@jorgensenfamily.us>
2020-11-04 15:52:04 -0800

    Object grabbing/throwing
    Added Object grabbing/throwing, pointer UI element, and most controller button mappings. (not run yet)

--- After Dawn/Assets/Scripts/GrabControl.cs
+++ After Dawn/Assets/Scripts/GrabControl.cs
@@ -3,6 +3,14 @@
 public class GrabControl : MonoBehaviour
 {
 	new Camera camera;
+	public float grabDistance;
+	public float throwStartDistance;
+	public float throwForce;
+	GameObject grabbed;
+	Rigidbody grabbedRigidbody;
+	bool holding;
+
+
 	private void Start()
 	{
 		camera = GetComponent<Camera>();
@@ -13,21 +21,40 @@ public class GrabControl : MonoBehaviour
 		Grabbing();
 	}
 
-	void Grabbing() //Controls calling of interaction functions
+	void Grabbing() //Controls grabbing of grabbable objects
 	{
-		if (Input.GetButtonDown("Interact"))
+		if (Input.GetButtonUp("Grab"))
 		{
-			Ray grab = camera.ScreenPointToRay(Input.mousePosition);
-			RaycastHit grabInfo;
+			if (!holding)
+			{ 
+				Ray grab = camera.ScreenPointToRay(Input.mousePosition);
+				RaycastHit grabInfo;
 
-			if (Physics.Raycast(grab, out grabInfo, 5))
-			{
-				if (grabInfo.collider.gameObject.tag == "Grabbable")
+
+				if (Physics.Raycast(grab, out grabInfo, grabDistance))
 				{
-					grabInfo.collider.gameObject.SetActive(false);
-					grabInfo.collider
-	= transform.position - Vector3.down;
+					if (grabInfo.collider.gameObject.tag == "Grabbable")
+					{
+						grabbed = grabInfo.collider.gameObject;
+						grabbedRigidbody = grabInfo.collider.attachedRigidbody;
+						grabbed.SetActive(false);
+						grabbed.transform.parent = transform;
+						
+						holding = true;
+					}
 				}
+			}
+			
+			else if (holding)
+			{
+				grabbed.transform.localPosition = new Vector3(0, 0, throwStartDistance);
+				grabbed.transform.rotation = transform.rotation;
+				grabbed.SetActive(true);
+				grabbed.transform.parent = null;
+				grabbedRigidbody.velocity = Vector3.zero;
+				grabbedRigidbody.angularVelocity = Vector3.zero;
+				grabbedRigidbody.AddRelativeForce(Vector3.forward * throwForce);
+				holding = false;
 			}
 		}
 	}
