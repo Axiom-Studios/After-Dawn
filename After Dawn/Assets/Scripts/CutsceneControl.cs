20334c1ec3e1b334044835cc43e46e514e794d1e
John Jorgensen <john@jorgensenfamily.us>
2020-12-12 17:22:29 -0800

    beginning scripts + more snowmen
    added more snowman as placeholders and began work on script (to be activated via function call)

--- After Dawn/Assets/Scripts/CutsceneControl.cs
+++ After Dawn/Assets/Scripts/CutsceneControl.cs
@@ -1,17 +1,13 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
+﻿using UnityEngine;
+using System.Collections;
 public class CutsceneControl : MonoBehaviour
 {
+	public GameObject sceneSprite;
 	
-    void Start()
-    {
-        
-    }
-
-    void Update()
-    {
-        
-    }
+	void Initialize(string name, int length){
+		int current = 1;
+		while (current < length){
+			sceneSprite = GameObject.Find("/Canvas/Cutscenes/snowman/");
+		}
+	}
 }
