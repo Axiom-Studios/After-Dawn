f640ca6fe6408ce5874f13687d84b856eb4dd548
b00mman <vincent.jude.clement@gmail.com>
2020-12-17 15:31:58 -0800

    Basic Orbs
    Added the basics for the sister orbs.

---
+++ After Dawn/Assets/Scripts/Orb.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orb : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            OrbManager.orbs += 1;
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ After Dawn/Assets/Scripts/OrbManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbManager : MonoBehaviour
+{
+    public static int orbs = 0;
+    public GameObject secret;
+    public static bool secretTriggered = false;
+    // Update is called once per frame
+    void Update()
+    {
+        if(orbs > 8 && !(secretTriggered))
+        {
+            SecretText();
+        }
+        if (secretTriggered && !(PauseMenu.paused))
+        {
+            secret.SetActive(false);
+        }
+    }
+    void SecretText()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        secret.SetActive(true);
+        Time.timeScale = 0f;
+        PauseMenu.paused = true;
+        secretTriggered = true;
+    }
+}
