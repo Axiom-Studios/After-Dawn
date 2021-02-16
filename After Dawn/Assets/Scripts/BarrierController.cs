bd91aedd2804b9073c2afd95ad7c993505703bcd
Vincent Clement <50496866+b00mman@users.noreply.github.com>
2021-02-16 08:35:18 -0800

    Add files via upload

---
+++ After Dawn/Assets/Scripts/BarrierController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarrierController : MonoBehaviour
+{
+    public int order;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (DayNightCycle.day == order)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
