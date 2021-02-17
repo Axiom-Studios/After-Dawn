748060cd503bffcc27fb538cbd3b2116bd7c1497
John Jorgensen <john@jorgensenfamily.us>
2021-02-17 14:19:22 -0800

    umm cool

--- After Dawn/Assets/Scripts/CutsceneControlM3.cs
+++ After Dawn/Assets/Scripts/CutsceneControlM3.cs
@@ -1,11 +1,10 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 public class CutsceneControlM3 : MonoBehaviour
 {
 	public List<GameObject> Scenes;
-    // Start is called before the first frame update
+	
     void Start()
     {
         
