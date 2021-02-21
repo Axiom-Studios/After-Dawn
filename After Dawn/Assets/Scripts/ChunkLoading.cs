ad27fdc6b50477d040141ff7f487b256d73a481f
John Jorgensen <john@jorgensenfamily.us>
2021-02-20 20:22:27 -0800

    might work probably

---
+++ After Dawn/Assets/Scripts/ChunkLoading.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChunkLoading : MonoBehaviour
+{
+	public List<GameObject> chunks;
+	public float renderDistance;
+	public Transform player;
+    void Update()
+    {
+        foreach (GameObject chunk in chunks){
+			float chunkPos = chunk.transform.position.x - chunk.transform.position.y - chunk.transform.position.z;
+			float playerPos = player.position.x - player.position.y - player.position.z;
+			if (Mathf.Sqrt(chunkPos + playerPos) > renderDistance){
+				chunk.SetActive(false);
+			}
+		}
+    }
+}
