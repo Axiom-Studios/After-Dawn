c4616a262e0f6b0ebd672a02a1723028108aa299
Jakob Nacanaynay <jnac8080@gmail.com>
2021-02-24 19:59:14 -0800

    Basic dialogue system added

---
+++ After Dawn/Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI dialogueText;
+    public GameObject dialogueBox;
+    List<string> sentencesQueue = new List<string>();
+    // Update is called once per frame
+    void Update()
+    {
+        if (sentencesQueue.Count == 0)
+        {
+            dialogueBox.SetActive(false);
+        }
+        else
+        {
+            dialogueText.text = sentencesQueue[0];
+            dialogueBox.SetActive(true);
+        }
+    }
+}
