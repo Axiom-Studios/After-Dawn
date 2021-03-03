066aae10fd591b754c9752c7793b8cf93187b2f7
Jakob Nacanaynay <jnac8080@gmail.com>
2021-03-03 14:06:53 -0800

    Add orb camera switch

---
+++ After Dawn/Assets/Scripts/SkyOrb.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyOrb : MonoBehaviour
+{
+    public Camera playerCamera;
+    public Camera skyCamera;
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            if (!DialogueManager.skyExplained)
+            {
+                DialogueManager.sentencesQueue.Add("Sky orbs allow you to see from the sky for 5 seconds");
+                DialogueManager.skyExplained = true;
+            }
+
+            StartCoroutine(switchForFive());
+        }
+    }
+
+    IEnumerator switchForFive()
+    {
+        playerCamera.enabled = false;
+        skyCamera.enabled = true;
+
+        yield return new WaitForSeconds(5);
+
+        skyCamera.enabled = false;
+        playerCamera.enabled = true;
+        Destroy(gameObject);
+    }
+}
