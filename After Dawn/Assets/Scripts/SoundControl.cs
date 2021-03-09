23fe181155865fbd91f7db3b6e09cd0acadaceb3
John Jorgensen <john@jorgensenfamily.us>
2021-03-09 13:35:12 -0800

    Walking
    Added walking sounds with variable rate

---
+++ After Dawn/Assets/Scripts/SoundControl.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundControl : MonoBehaviour
+{
+	public List<AudioClip> footsteps;
+	public float rate;
+	float speed;
+	float lastStep = 0;
+	public AudioSource stepSource;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Player.keys != Vector3.zero){
+			if ((Time.time - lastStep) >= rate){
+				stepSource.clip = footsteps[Random.Range(0, footsteps.Count - 1)];
+				stepSource.Play();
+				lastStep = Time.time;
+			}
+		}
+    }
+}
