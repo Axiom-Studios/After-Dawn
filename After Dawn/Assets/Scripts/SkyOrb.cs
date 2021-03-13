74ce46ce69e48c1af1f7c74556e382fd5ef20f53
Jakob Nacanaynay <jnac8080@gmail.com>
2021-03-12 19:52:14 -0800

    UI edits

--- After Dawn/Assets/Scripts/SkyOrb.cs
+++ After Dawn/Assets/Scripts/SkyOrb.cs
@@ -1,11 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class SkyOrb : MonoBehaviour
 {
     public Camera playerCamera;
     public Camera skyCamera;
+    public Image pointer;
     public static bool skyActive = false;
     float lookTime = 7f;
     public bool collected = false;
@@ -14,6 +15,8 @@ public class SkyOrb : MonoBehaviour
     {
         playerCamera = GameObject.Find("Main Camera").GetComponent<Camera>();
         skyCamera = GameObject.Find("Sky Camera").GetComponent<Camera>();
+        pointer = GameObject.Find("Pointer").GetComponent<Image>();
+        pointer.enabled = false;
 		source = gameObject.GetComponent<AudioSource>();
     }
 
@@ -56,11 +59,13 @@ public class SkyOrb : MonoBehaviour
     {
         playerCamera.enabled = false;
         skyCamera.enabled = true;
+        pointer.enabled = true;
 
         yield return new WaitForSeconds(lookTime);
 
         skyCamera.enabled = false;
         playerCamera.enabled = true;
         skyActive = false;
+        pointer.enabled = false;
     }
 }
